0d9816499d573cd5c5a4bd5d87a2a949ac28bada
Dang Nguyen <danghwin@gmail.com>
2020-05-11 16:48:26 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,29 @@
-# Write your code here.
+
+
+def line(katz_deli)
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+  else
+   new_message="The line is currently:"
+
+    katz_deli.each_with_index do |value, index|
+      new_message += " #{index.to_i+1}. #{value}"
+    end
+
+    puts "#{new_message}"
+  end
+  
+def take_a_number(katz_deli, name)
+  katz_deli << name
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+end
+
+def now_serving(katz_deli)
+  if katz_deli.length == 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{katz_deli.shift}."
+  end
+
+
+end
